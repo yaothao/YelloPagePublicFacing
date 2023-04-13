18c53da1fd0aa7cdb9365b7a052ec81a57a4944c
yaothao <ty39@uw.edu>
2023-04-13 13:27:31 -0700

    fix merge issue

--- src/Homebar.js
+++ src/Homebar.js
@@ -5,12 +5,8 @@ export default function Homebar({ handleAboutClicked }) {
     // the top welcome notes stays throughout the webpages 
     return (
         <div className="web-title">
-<<<<<<< HEAD
-            Welcome to Yellow Page Archived WebSite
+            Chinese-language Websites at the Millennium            
             <button onClick={() => handleAboutClicked()}>About</button>
-=======
-            Chinese-language Websites at the Millennium
->>>>>>> 50fa6a7ea1b934791c42fddd0ca93ab94ddc5557
         </div>
     )
 }
