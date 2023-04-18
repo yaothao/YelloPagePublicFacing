ecc9d0aea597aaa8741a763f299007c9b7e08c26
rlwhuang <lh1677@nyu.edu>
2023-04-18 00:24:53 -0700

    update logo, homebar, disclaim. and about

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import React from "react";
 function About({backBotton}) {
     return (
         <div className="about-page">
-            <h1>This is the about page</h1> 
+            <h1>About</h1> 
             <p>This website present you a variaty of archived Chinese Websites</p>
         </div>
              
--- src/Homebar.js
+++ src/Homebar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './Homebar.css'
 import { Link } from 'react-router-dom'
+import sitelogo from './sitelogo.svg'
 
 export default function Homebar() {
     // the top welcome notes stays throughout the webpages 
     return (
         <div className="web-title">
             <ul className="menu-list">
-                <li>Chinese-language Websites at the Millennium</li>
+                <li><img src={sitelogo} alt="Logo" /></li>
                 <li className="menu-item"><Link to='/'>Home</Link></li>
                 <li>
                     <Link to='/about'>About</Link>
